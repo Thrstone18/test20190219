17ab08a67dd6d315849edfebf971051154a9eeb0
LeiZhang <leizhang1005@163.com>
2019-02-19 10:39:13 +0800

    my first commit

---
+++ src/DeleteTest.java
@@ -0,0 +1,19 @@
+public class DeleteTest {
+    public static void main(String[] args) {
+
+
+
+//        OkHttpClient client = new OkHttpClient();
+//        Request request = new Request.Builder()
+//                .url("http://223.167.110.4:8000/m2m/endpoints/999998888877777")
+//                .delete(null)
+//                .addHeader("Authorization", "Basic aGRxeHNtb2tlOkhkcXhzbW9rZUAwMjE=")
+//                .addHeader("Content-Type", "application/json")
+//                .addHeader("Accept", "application/json")
+//                .addHeader("cache-control", "no-cache")
+//                .addHeader("Postman-Token", "174335b3-db80-4a75-afe3-edda7cf32bf6")
+//                .build();
+//        Response response = client.newCall(request).execute();
+
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+//        HelloWorld helloWorld=new HelloWorld();
+//        helloWorld.setName("Spring");
+//        helloWorld.sayHello();
+//        ApplicationContext context=new ClassPathXmlApplicationContext("SpringBoot001.xml");
+//        HelloWorld helloWorld= (HelloWorld) context.getBean("helloWorld");
+//        helloWorld.sayHello();
+
+    }
+}
